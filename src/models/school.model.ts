b46efe7fe78ad03597195d8a1e89836abe1218b5
Dhruvil Shah <dhruvilshah884@gmail.com>
2025-05-08 22:36:34 +0530

    add school and user model

---
+++ src/models/school.model.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const SchoolSchema = new mongoose.Schema({
+    name: {
+        type: String,   
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    logo: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true,
+    versionKey: false,
+});
+export const SchoolModel = mongoose.models.School || mongoose.model('School', SchoolSchema);
---
+++ src/models/user.model.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },  
+    gender:{
+        type: String,
+        required: true,
+    },
+    role:{
+        type: String,
+        enum: ['ADMIN', 'PRINCIPAL', 'TEACHER', 'PARENT', 'STUDENT', 'RECEPTION', 'SECURITY'],
+        required: true,
+    },
+    profile_image:{
+        type: String,
+        required: true,
+    },
+    school:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'School',
+        required: true,
+    },
+}, {
+    timestamps: true,
+    versionKey: false,
+});
+export const UserModel = mongoose.models.User || mongoose.model('User', UserSchema);
